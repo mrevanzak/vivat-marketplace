afe2e8bc17795426b173454ee7916847597ef00b
mrevanzak <mrevanzak@gmail.com>
2023-11-05 21:28:56 +0700

    feat(web): simple landing page for debug

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,31 +1,24 @@
-// import { AuthShowcase } from "./_components/auth-showcase";
+"use client";
+
+import { SignIn, SignOutButton, useUser } from "@clerk/nextjs";
+
+import { api } from "~/utils/api";
 
 export const runtime = "edge";
 
 export default function HomePage() {
+  const { isSignedIn } = useUser();
+  const { data } = api.product.showProduct.useQuery({
+    id: "313501f1-7be4-11ee-a239-7a04ac76345d",
+  });
+  console.log(data);
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-pink-400">T3</span> Turbo
         </h1>
-        {/* <SignIn /> */}
-        {/* <AuthShowcase /> */}
-
-        {/* <CreatePostForm />
-        <div className="h-[40vh] w-full max-w-2xl overflow-y-scroll">
-          <Suspense
-            fallback={
-              <div className="flex w-full flex-col gap-4">
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-              </div>
-            }
-          >
-            <PostList />
-          </Suspense>
-        </div> */}
+        {isSignedIn ? <SignOutButton /> : <SignIn />}
       </div>
     </main>
   );
