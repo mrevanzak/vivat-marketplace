48f1ddca22f3e4bce273f3349ea2f51cd3029a16
mrevanzak <mrevanzak@gmail.com>
2023-11-29 13:14:26 +0700

    docs: env

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -9,6 +9,9 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  },
 };
 
 export default config;
